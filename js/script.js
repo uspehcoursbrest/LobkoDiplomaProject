1740115b11fa6ff5bfb52ac4d554346993abbef9
DmitriLobko <dmitrilobko1995@tut.by>
2019-03-31 05:24:27 +0300

    Подключил JS и jQuery,внес правки

---
+++ js/script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    function clock() {
+        var date = new Date(),
+            hour = date.getHours(),
+            min = date.getMinutes(),
+            second = date.getSeconds();
+        if (hour < 10) hour = "0" + hour;
+        if (min < 10) min = "0" + min;
+        if (second < 10) second = "0" + second;
+        document.getElementById("period").innerHTML = hour + ":" + min + ":" + second;
+    }
+    var timer;
+    function  clockStart() {
+        timer =setInterval(clock, 1000);
+        clock();
+    }
+    clockStart()
+}

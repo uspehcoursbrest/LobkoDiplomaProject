5eb0792fb3588220531c9e880a94d3064de45051
DmitriLobko <dmitrilobko1995@tut.by>
2019-04-03 08:03:10 +0300

    Внес правки

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-window.onload = function() {
-    function clock() {
-        var date = new Date(),
-            hour = date.getHours(),
-            min = date.getMinutes(),
-            second = date.getSeconds();
-        if (hour < 10) hour = "0" + hour;
-        if (min < 10) min = "0" + min;
-        if (second < 10) second = "0" + second;
-        document.getElementById("period").innerHTML = hour + ":" + min + ":" + second;
+$(document).ready(function () {
+    window.onload = function () {
+        function clock() {
+            var date = new Date(),
+                hour = date.getHours(),
+                min = date.getMinutes(),
+                second = date.getSeconds();
+            if (hour < 10) hour = "0" + hour;
+            if (min < 10) min = "0" + min;
+            if (second < 10) second = "0" + second;
+            document.getElementById("period").innerHTML = hour + ":" + min + ":" + second;
+        }
+
+        var timer;
+
+        function clockStart() {
+            timer = setInterval(clock, 1000);
+            clock();
+        }
+
+        clockStart()
     }
-    var timer;
-    function  clockStart() {
-        timer =setInterval(clock, 60000);
-        clock();
-    }
-    clockStart()
-}
+})
